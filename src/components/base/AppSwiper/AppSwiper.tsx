68db7d7518cc91a32f66a7f4a159794b49152b9d
qieman <253058016@qq.com>
2023-08-14 10:02:29 +0800

    Update AppSwiper.tsx

--- src/components/base/AppSwiper/AppSwiper.tsx
+++ src/components/base/AppSwiper/AppSwiper.tsx
@@ -1,6 +1,6 @@
-import { ReactNode } from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Virtual } from 'swiper'
+import {ReactNode} from 'react'
+import {Swiper, SwiperSlide, useSwiper} from 'swiper/react'
+import {Virtual} from 'swiper'
 import 'swiper/css/virtual';
 import {Swiper as SwiperClass} from "swiper/types";
 
@@ -8,16 +8,32 @@ interface AppSwiperProps {
     items: ReactNode[]
     space: number,
     itemWidth: number,
-    initIndex?:number,
+    initIndex?: number,
     endEnhancer?: ReactNode
+}
+
+function Wrapper(props: { children: ReactNode, index: number, width: number, space: number }) {
+    const swiper = useSwiper()
+    return <div className='swiper-inside-wrapper'  style={{width: props.width + 'px'}} onClick={e => {
+        setTimeout(() => {
+            const windowWidth = window.innerWidth
+            const offset = Math.floor(windowWidth / (props.width + props.space) / 2)
+            const index = props.index - offset
+            console.log('swiper', swiper)
+            console.log('offset', offset)
+            console.log('index', index)
+            swiper.slideTo(index, 500, false)
+        }, 0)
+    }}>{props.children}</div>
+
 }
 
 function AppSwiper(props: AppSwiperProps) {
     return (<Swiper
         data-testid='AppSwiper'
         modules={[Virtual]}
-        spaceBetween={ props.space }
-        freeMode={ true }
+        spaceBetween={props.space}
+        freeMode={true}
         onSwiper={
             (swiper: SwiperClass) => {
                 if (props.initIndex) {
@@ -25,13 +41,20 @@ function AppSwiper(props: AppSwiperProps) {
                 }
             }
         }
-        slidesPerView={'auto'} >
-        { props.items.map((item, index) => {
-            return <SwiperSlide style={{width: props.itemWidth + 'px'}} key={index} virtualIndex={index}>{ item }</SwiperSlide>
+        slidesPerView={'auto'}>
+        {props.items.map((item, index) => {
+            return <SwiperSlide style={{width: props.itemWidth + 'px'}} key={index} virtualIndex={index}>
+                <Wrapper
+                    space={props.space}
+                    index={index}
+                    width={props.itemWidth}>
+                    {item}
+                </Wrapper>
+            </SwiperSlide>
         })}
 
         {
-            <SwiperSlide style={{width: 10 + 'px'}}>{ props.endEnhancer }</SwiperSlide>
+            <SwiperSlide style={{width: 10 + 'px'}}>{props.endEnhancer}</SwiperSlide>
         }
     </Swiper>)
 }
