90f2f8611ca4896f5dd56faf406e6ee39b8551a1
qieman <253058016@qq.com>
2023-09-22 11:34:21 +0800

    Update ListEventVertical.tsx

--- src/components/compose/ListEventVertical/ListEventVertical.tsx
+++ src/components/compose/ListEventVertical/ListEventVertical.tsx
@@ -15,10 +15,11 @@ import userContext from "../../provider/UserProvider/UserContext";
 import EventHomeContext from "../../provider/EventHomeProvider/EventHomeContext";
 import useTime from '../../../hooks/formatTime'
 
-
+let MapLibReady = false
+let MapError = false;
 
 // @ts-ignore
-(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.onload=()=>{MapLibReady=true};a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>{MapError=true;h=n(Error(p+" could not load."))};a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
     key: import.meta.env.VITE_GMAP_API_KEY,
     v: "weekly",
     // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
@@ -45,6 +46,8 @@ function ListEventVertical() {
     const [searchKeyword, setSearchKeyWork] = useState<string>('')
     const [mode, setMode] = useState<'list' | 'map'>(searchParams.get('mode') === 'map' ? 'map' : 'list')
     const [mapReady, setMapReady] = useState(false)
+    const [mapLibReady, setMapLibReady] = useState(MapLibReady)
+    const [mapLibError, setMapLibError] = useState(MapError)
     const [selectedEventInMap, setSelectedEventInMap] = useState<null | Event>(null)
     const [compact, setCompact] = useState(true)
 
@@ -127,7 +130,7 @@ function ListEventVertical() {
             }
         } catch (e: any) {
             console.error(e)
-            showToast(e.message)
+            // showToast(e.message)
             return []
         } finally {
             unload()
@@ -153,6 +156,15 @@ function ListEventVertical() {
     useEffect(() => {
         setSearchParams({'mode': mode})
     }, [mode])
+
+    useEffect(() => {
+        if (MapLibReady) {
+            setMapLibReady(true)
+        }
+        if (mapLibError) {
+            setMapLibError(true)
+        }
+    }, [MapLibReady, mapLibError])
 
     useEffect(() => {
       if (list.length && mapReady) {
@@ -316,18 +328,22 @@ function ListEventVertical() {
                         </div>
                     }
 
-                    <div className={'mode-switch'}>
-                        <div className={'switcher'}>
-                            <div onClick={() => {setTab2Index('soon');setMode('map')}}
-                                 className={mode === 'map' ? 'switcher-item active': 'switcher-item'}>
-                                <i className={'icon-location'} />
-                            </div>
-                            <div onClick={() => {setMode('list')}}
-                                 className={mode === 'list' ? 'switcher-item active': 'switcher-item'}>
-                                <i className={'icon-menu'} />
+
+                    { mapLibReady &&
+                        <div className={'mode-switch'}>
+                            <div className={'switcher'}>
+                                <div onClick={() => {setTab2Index('soon');setMode('map')}}
+                                     className={mode === 'map' ? 'switcher-item active': 'switcher-item'}>
+                                    <i className={'icon-location'} />
+                                </div>
+                                <div onClick={() => {setMode('list')}}
+                                     className={mode === 'list' ? 'switcher-item active': 'switcher-item'}>
+                                    <i className={'icon-menu'} />
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    }
+
                 </div>
                 { !!eventGroup && eventGroup.group_event_tags && mode === 'map' && !compact &&
                 <div className={'center'}>
