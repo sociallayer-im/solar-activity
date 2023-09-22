96682bb6028d55d8a886f57fb890a20a23c24280
qieman <253058016@qq.com>
2023-09-22 21:22:19 +0800

    fix calendar scroll bug

--- src/pages/CalendarNew/CalendarNew.tsx
+++ src/pages/CalendarNew/CalendarNew.tsx
@@ -119,7 +119,6 @@ function Calendar() {
 
             setCurrMonthEventList(eventWithProfile)
             cache.set(dateStart.toISOString() + eventGroup?.username, eventWithProfile)
-
         }
     }
 
