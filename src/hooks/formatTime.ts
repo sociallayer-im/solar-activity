10c6c0de86168d07b4c3f2e0e927e1a28183e5c3
qieman <253058016@qq.com>
2023-09-24 22:09:05 +0800

    update time  format

--- src/hooks/formatTime.ts
+++ src/hooks/formatTime.ts
@@ -8,8 +8,22 @@ const formatTime = (dateString: string) => {
     return `${year}.${mon.padStart(2, '0')}.${date.padStart(2, '0')} ${hour.padStart(2, '0')}:${min.padStart(2, '0')}`
 }
 
+const formatTime2 = (dateString: string) => {
+    // format like:THU, SEP 26 AT 9 PM
+    const dateObject = new Date(dateString)
+    const week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+    const month = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL',
+        'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+    const mon = month[dateObject.getMonth()]
+    const date = dateObject.getDate() + ''
+    const hour = dateObject.getHours() >  12 ? dateObject.getHours() - 12 + '' : dateObject.getHours() + ''
+    const min = dateObject.getMinutes() + ''
+    const amOrPm = dateObject.getHours() > 12 ? 'PM' : 'AM'
+    return `${week[dateObject.getDay()]}, ${mon} ${date.padStart(2, '0')} AT ${hour.padStart(2, '0')}:${min.padStart(2, '0')} `  + amOrPm
+}
+
 function useTime () {
-    return formatTime
+    return formatTime2
 }
 
 export default useTime
